ce030bca88f37416dd99c93ceff09db97913677e
anishpatel691 <youtubanish@gmail.com>
2025-03-02 00:51:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000', // Proxy requests from /api to backend server
-    },
-  },
+  base: '/myapp/', // Change this to match your deployment path
   plugins: [react()],
 })
