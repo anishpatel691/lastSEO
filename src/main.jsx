f73dbc200b5659a35e68399bd984f34a5c62382e
anishpatel691 <youtubanish@gmail.com>
2025-03-02 01:10:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import YouTubeSEOAnalyzer from './components/YT/YouTubeSEOAnalyzer'
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
 <>   
     {/* <ChatComponent/> */}
- <YouTubeSEOAnalyzer/>
+<App/>
     </>
 )
